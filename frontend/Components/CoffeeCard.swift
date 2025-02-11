5f089b02ea216a63d91fa3ce3160d988e15dc864
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-02-10 21:59:51 -0800

    Added skeleton for coffeecard component

---
+++ frontend/Components/CoffeeCard.swift
@@ -0,0 +1,18 @@
+//
+//  CoffeeCard.swift
+//  DialedIn
+//
+//  Created by Hunter Tratar on 2/10/25.
+//
+
+import SwiftUI
+
+struct CoffeeCard: View {
+    var body: some View {
+        Text("Coffee Card")
+    }
+}
+
+#Preview {
+    return CoffeeCard()
+}
