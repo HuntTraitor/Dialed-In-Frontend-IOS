2a93ffabc488870f9c238e11fe5f99fc97b8ade3
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-02-15 19:39:06 -0800

    Added a smaller coffeecard for viewing coffees in a list

---
+++ frontend/Components/CoffeeCardSmall.swift
@@ -0,0 +1,19 @@
+//
+//  CoffeeCardSmall.swift
+//  DialedIn
+//
+//  Created by Hunter Tratar on 2/15/25.
+//
+
+import SwiftUI
+
+struct CoffeeCardSmall: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CoffeeCardSmall()
+}
+
