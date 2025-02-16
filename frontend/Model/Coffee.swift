81067584b5b6d908d5b5806c8f9010bf043cbaff
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-02-16 12:48:57 -0800

    Added process to coffee model

--- frontend/Model/Coffee.swift
+++ frontend/Model/Coffee.swift
@@ -11,6 +11,7 @@ struct Coffee: Identifiable, Codable, Hashable {
     let id: Int
     let name: String
     let region: String
+    let process: String
     let description: String
     let imgURL: String
     
@@ -18,11 +19,12 @@ struct Coffee: Identifiable, Codable, Hashable {
         case id
         case name
         case region
+        case process
         case description
         case imgURL
     }
 }
 
 extension Coffee {
-    static var MOCK_COFFEE = Coffee(id: 1, name: "Milky Cake", region: "Columbia", description: "This is a delicious sweet coffee that has notes of caramel and chocolate.", imgURL: "https://st.kofio.co/img_product/boeV9yxzHn2OwWv/9628/sq_350_MFbecow28XW0zprTGaVA_102573.png")
+    static var MOCK_COFFEE = Coffee(id: 1, name: "Milky Cake", region: "Columbia", process: "Thermal Shock", description: "This is a delicious sweet coffee that has notes of caramel and chocolate.", imgURL: "https://st.kofio.co/img_product/boeV9yxzHn2OwWv/9628/sq_350_MFbecow28XW0zprTGaVA_102573.png")
 }
