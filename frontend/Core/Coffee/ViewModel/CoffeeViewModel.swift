3f14511ded86e7b05dcd2f1598e5e1b50a01b926
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-03-03 21:46:08 -0800

    Added postcoffee endpoint using multipart form data

--- frontend/Core/Coffee/ViewModel/CoffeeViewModel.swift
+++ frontend/Core/Coffee/ViewModel/CoffeeViewModel.swift
@@ -24,5 +24,24 @@ class CoffeeViewModel: ObservableObject {
         let coffees = try decoder.decode([Coffee].self, from: jsonData)
         return coffees
     }
+    
+    func postCoffee(input: CoffeeInput, token: String) async throws {
+        let url = URL(string: "http://localhost:3000/v1/coffees")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        let boundary = "Boundary-\(UUID().uuidString)"
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+
+        let postData = input.toMultiPartData(boundary: boundary)
+        request.httpBody = postData
+
+        let (_, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 201 else {
+            throw CustomError.methodError(message: "Failed to upload coffee")
+        }
+    }
 
 }
