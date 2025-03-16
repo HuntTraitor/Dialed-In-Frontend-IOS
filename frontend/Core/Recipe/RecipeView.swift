2af38e528473607a6c8bd84b2185676fc18f7b14
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-03-16 09:36:14 -0700

    Added a clock to time pours

--- frontend/Core/Recipe/RecipeView.swift
+++ frontend/Core/Recipe/RecipeView.swift
@@ -5,3 +5,18 @@
 //  Created by Hunter Tratar on 3/15/25.
 //
 
+import SwiftUI
+
+public struct RecipeView: View {
+    public var body: some View {
+        Text("Recipe for: Coffee")
+        if case let .switchRecipe(recipeData) = Recipe.MOCK_SWITCH_RECIPE.info,
+           let firstPhase = recipeData.phases[1] {
+            CountdownTimer(seconds: firstPhase.time)
+        }
+    }
+}
+
+#Preview {
+    RecipeView()
+}
