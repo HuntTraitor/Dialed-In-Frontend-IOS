63b50ab91e9762a4830b6210f101207f3b27e210
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-03-18 14:11:25 -0700

    Created skeleton for the recipe card

--- frontend/Core/Recipe/RecipeCard.swift
+++ frontend/Core/Recipe/RecipeCard.swift
@@ -5,3 +5,14 @@
 //  Created by Hunter Tratar on 3/15/25.
 //
 
+import SwiftUI
+
+struct RecipeCard: View {
+    var body: some View {
+        Text("Recipe Card")
+    }
+}
+
+#Preview {
+    RecipeCard()
+}
