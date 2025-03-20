654500ca80e8385a771a1bd5d395a5f86633c46c
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-03-20 11:00:27 -0700

    Added a recipe card view

--- frontend/Core/Recipe/RecipeCard.swift
+++ frontend/Core/Recipe/RecipeCard.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct RecipeCard: View {
+    var recipe: SwitchRecipe
+    var coffee: Coffee
     var body: some View {
-        Text("Recipe Card")
+        let totalTime = recipe.info.phases.reduce(0) { $0 + $1.value.time }
+        HStack {
+            VStack {
+                ImageView(URL(string: coffee.img!))
+                Text(coffee.name)
+                    .bold()
+            }
+            VStack {
+                Text(recipe.info.name)
+                    .italic()
+                    .bold()
+                    .padding(.bottom, 10)
+                HStack {
+                    Image(systemName: "cup.and.heat.waves.fill")
+                        .foregroundColor(Color("background"))
+                    Text("\(recipe.info.gramsIn)g")
+                    Spacer()
+                    Image(systemName: "drop.fill")
+                        .foregroundColor(Color("background"))
+                    Text("\(recipe.info.mlOut)ml")
+                }
+                .padding(.bottom, 10)
+                HStack {
+                    Image(systemName: "clock")
+                        .foregroundColor(Color("background"))
+                    Text(String(format: "%d:%02d", totalTime / 60, totalTime % 60))
+                        .bold()
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    RecipeCard()
+    RecipeCard(recipe: SwitchRecipe.MOCK_SWITCH_RECIPE, coffee: Coffee.MOCK_COFFEE)
 }
