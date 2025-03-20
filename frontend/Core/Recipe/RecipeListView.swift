788a91a26f0d2ec1aa8b4fe52e8649c59d41928e
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-03-20 11:26:51 -0700

    Added a recipe list view

--- frontend/Core/Recipe/RecipeListView.swift
+++ frontend/Core/Recipe/RecipeListView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct RecipeListView: View {
+    @EnvironmentObject var keychainManager: KeychainManager
+    @EnvironmentObject var coffeeModel: CoffeeViewModel
+    @State private var recipeList: [SwitchRecipe] = [SwitchRecipe.MOCK_SWITCH_RECIPE, SwitchRecipe.MOCK_SWITCH_RECIPE, SwitchRecipe.MOCK_SWITCH_RECIPE]
+    @Bindable private var navigator = NavigationManager.nav
+    @State private var searchTerm = ""
     let curMethod: Method
+    
+    var filteredRecipes: [SwitchRecipe] {
+        guard !searchTerm.isEmpty else { return recipeList }
+        return recipeList.filter {$0.info.name.localizedCaseInsensitiveContains(searchTerm)}
+    }
+    
     var body: some View {
-        Text("List of recipes for \(curMethod.name)")
+        NavigationStack(path: $navigator.mainNavigator) {
+            VStack {
+                HStack {
+                    Text("Recipes")
+                        .font(.title)
+                        .italic()
+                        .underline()
+                        .padding(.top, 40)
+                        .padding(.bottom, 10)
+                        .padding(.leading, 30)
+                    Spacer()
+                }
+                SearchBar(text: $searchTerm, placeholder: "Search Recipes")
+                ScrollView {
+                    ForEach(filteredRecipes, id: \.self) { recipe in
+                        NavigationLink(
+                            destination: RecipeView(
+                                recipe: recipe,
+                                coffee: Coffee.MOCK_COFFEE
+                            )
+                            .environmentObject(keychainManager)
+                            .environmentObject(coffeeModel)
+                        ) {
+                            RecipeCard(recipe: recipe, coffee: Coffee.MOCK_COFFEE)
+                                .frame(maxWidth: .infinity, maxHeight: 120)
+                                .padding()
+                                .background(Color(.systemBackground))
+                                .cornerRadius(15)
+                                .shadow(radius: 2)
+                                .padding(.horizontal, 20)
+                                .padding(.vertical, 3)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+            }
+        }
     }
 }
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
+        let keychainManager = KeychainManager()
+        let coffeeViewModel = CoffeeViewModel()
         RecipeListView(curMethod: Method(id: 1, name: "Pour Over"))
+            .environmentObject(keychainManager)
+            .environmentObject(coffeeViewModel)
     }
 }
