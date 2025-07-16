a883cdc66269c2852db182a1ae13fe5e01d3b9a7
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-07-16 15:36:19 -0700

    Added a little white arrow to indicate card is clickable

--- frontend/Core/Method/View/MethodCard.swift
+++ frontend/Core/Method/View/MethodCard.swift
@@ -9,28 +9,46 @@ import SwiftUI
 
 struct MethodCard: View {
     let title: String
-    let image: String   
+    let image: String
+
     var body: some View {
         ZStack {
             Image(image)
                 .resizable()
                 .scaledToFill()
-                .clipped()
                 .frame(width: 300, height: 175)
                 .clipShape(RoundedRectangle(cornerRadius: 15))
+                .clipped()
+
             Text(title)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundColor(.white)
                 .padding()
                 .shadow(color: .black, radius: 2, x: 2, y: 2)
+
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(systemName: "chevron.right.circle.fill")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .shadow(radius: 2)
+                        .padding(.trailing, 10)
+                        .padding(.bottom, 20)
+                }
+            }
         }
         .frame(width: 300, height: 200)
         .cornerRadius(15)
         .shadow(radius: 5)
-        .contentShape( Rectangle())
+        .contentShape(Rectangle())
     }
 }
+
+
+
 
 
 struct MethodCardPreviews: PreviewProvider {
