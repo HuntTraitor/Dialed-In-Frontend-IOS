4bd40e5437b24014aad3de336bcf231bc090c195
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-08-24 00:40:58 -0700

    Fixed Loading cirlce appearing beneath the coffee cards

--- frontend/Core/Coffee/View/CoffeeView.swift
+++ frontend/Core/Coffee/View/CoffeeView.swift
@@ -24,92 +24,92 @@ struct CoffeeView: View {
     }
     
     var body: some View {
-            ZStack {
-                Color(.systemGray6)
-                    .edgesIgnoringSafeArea(.all)
-                VStack {
-                    HStack {
-                        Text("Coffees")
-                            .font(.title)
-                            .italic()
-                            .padding(.top, 40)
-                            .padding(.bottom, 10)
-                            .padding(.leading, 30)
-                            .accessibilityIdentifier(testingID.coffeesTitle)
-                        
-                        Spacer()
-                        
-                        Button {
-                            isShowingCreateCoffeeView = true
-                        } label: {
-                            Label("Add New Coffee", systemImage: "plus")
-                                .font(.system(size: 15))
-                                .bold()
-                                .padding(.trailing, 30)
-                        }
-                        .sheet(isPresented: $isShowingCreateCoffeeView) {
-                            CreateCoffeeView()
-                        }
+        ZStack {
+            Color(.systemGray6)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    Text("Coffees")
+                        .font(.title)
+                        .italic()
                         .padding(.top, 40)
-                    }
+                        .padding(.bottom, 10)
+                        .padding(.leading, 30)
+                        .accessibilityIdentifier(testingID.coffeesTitle)
                     
-                    if viewModel.errorMessage != nil {
-                        FetchErrorMessageScreen(errorMessage: viewModel.errorMessage)
-                            .scaleEffect(0.8)
-                            .offset(y: -(UIScreen.main.bounds.height) * 0.1)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    } else if viewModel.coffees.isEmpty {
-                        NoResultsFound(itemName: "coffee", systemImage: "cup.and.heat.waves")
-                            .scaleEffect(0.8)
-                            .offset(y: -(UIScreen.main.bounds.height) * 0.1)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    } else {
-                        VStack {
-                            HStack {
-                                SearchBar(text: $searchTerm, placeholder: "Search Coffees")
-                                    .padding(.horizontal, 10)
-                                Button(action: {
-                                    isMinimized.toggle()
-                                }) {
-                                    Image(systemName: isMinimized ? "chevron.up" : "chevron.down")
-                                }
+                    Spacer()
+                    
+                    Button {
+                        isShowingCreateCoffeeView = true
+                    } label: {
+                        Label("Add New Coffee", systemImage: "plus")
+                            .font(.system(size: 15))
+                            .bold()
+                            .padding(.trailing, 30)
+                    }
+                    .sheet(isPresented: $isShowingCreateCoffeeView) {
+                        CreateCoffeeView()
+                    }
+                    .padding(.top, 40)
+                }
+                
+                if viewModel.errorMessage != nil {
+                    FetchErrorMessageScreen(errorMessage: viewModel.errorMessage)
+                        .scaleEffect(0.8)
+                        .offset(y: -(UIScreen.main.bounds.height) * 0.1)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                } else if viewModel.coffees.isEmpty {
+                    NoResultsFound(itemName: "coffee", systemImage: "cup.and.heat.waves")
+                        .scaleEffect(0.8)
+                        .offset(y: -(UIScreen.main.bounds.height) * 0.1)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                } else {
+                    VStack {
+                        HStack {
+                            SearchBar(text: $searchTerm, placeholder: "Search Coffees")
+                                .padding(.horizontal, 10)
+                            Button(action: {
+                                isMinimized.toggle()
+                            }) {
+                                Image(systemName: isMinimized ? "chevron.up" : "chevron.down")
                             }
-                            .padding(.bottom, 5)
-                            
-                            if filteredCoffees.isEmpty && !searchTerm.isEmpty {
-                                NoSearchResultsFound(itemName: "coffee")
-                                    .scaleEffect(0.8)
-                                    .offset(y: -(UIScreen.main.bounds.height) * 0.1)
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            } else {
-                                ScrollView {
-                                    ForEach(filteredCoffees, id: \.id) { coffee in
-                                        VStack {
-                                            CoffeeRow(coffee: coffee, isMinimized: $isMinimized)
-                                        }
-                                        .padding(.vertical, 10)
-                                        .background(Color.white)
-                                        .cornerRadius(10)
-                                        .shadow(color: .black.opacity(0.01), radius: 5, x: 0, y: 2)
+                        }
+                        .padding(.bottom, 5)
+                        
+                        if filteredCoffees.isEmpty && !searchTerm.isEmpty {
+                            NoSearchResultsFound(itemName: "coffee")
+                                .scaleEffect(0.8)
+                                .offset(y: -(UIScreen.main.bounds.height) * 0.1)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        } else {
+                            ScrollView {
+                                ForEach(filteredCoffees, id: \.id) { coffee in
+                                    VStack {
+                                        CoffeeRow(coffee: coffee, isMinimized: $isMinimized)
                                     }
+                                    .padding(.vertical, 10)
+                                    .background(Color.white)
+                                    .cornerRadius(10)
+                                    .shadow(color: .black.opacity(0.01), radius: 5, x: 0, y: 2)
                                 }
                             }
                         }
                     }
                 }
-                .frame(minHeight: 0, maxHeight: .infinity, alignment: .top)
-                .padding(.horizontal)
-                .addNavigationSupport()
-                .task {
-                    if !hasAppeared {
-                        await viewModel.fetchCoffees(withToken: authViewModel.token ?? "")
-                        hasAppeared = true
-                    }
+            }
+            .frame(minHeight: 0, maxHeight: .infinity, alignment: .top)
+            .padding(.horizontal)
+            .addNavigationSupport()
+            .task {
+                if !hasAppeared {
+                    await viewModel.fetchCoffees(withToken: authViewModel.token ?? "")
+                    hasAppeared = true
                 }
             }
             if viewModel.isLoading {
                 LoadingCircle()
             }
+        }
     }
 }
 
